45fadad8f0a707ec4c164dcb9fa1481d1bc2dba9
Juan Sanin <juanpablosaninb@gmail.com>
2020-11-07 19:33:27 -0500

    Main class created

---
+++ src/view/Main.java
@@ -0,0 +1,21 @@
+package view;
+
+/**
+ * Main class 
+
+ * @author: Juan P. Sanin
+
+ * @version: 1.0 11/7/2020
+
+ */
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

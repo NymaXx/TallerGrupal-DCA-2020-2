bcfd148bc83cd01ed9c5967d088e9600bb4f9d5c
Hillary22 <70818275+Hillary22@users.noreply.github.com>
2020-11-14 22:36:28 -0500

    Added message error in ContactRegister

--- src/view/ContactRegisterView.java
+++ src/view/ContactRegisterView.java
@@ -16,6 +16,7 @@ import processing.core.PImage;
 public class ContactRegisterView {
 	private Controller controller;
 	private PImage registerContactScreen;
+	private PImage errorMessageC;
 	private PImage addContactButton, addContactButtonUI, navBar,added;
 	private boolean contactAdded;
 	private PApplet app;
@@ -35,6 +36,7 @@ public class ContactRegisterView {
 		controller=new Controller();
 		this.app = app;
 		registerContactScreen = app.loadImage("../image/screens/newContactScreenn.png");
+		errorMessageC = app.loadImage("../image/interactive/registerContactErrorM.png");
 		navBar = app.loadImage("../image/interactive/addContactNavBar.png");
 		addContactButton= app.loadImage("../image/interactive/addContactButton.png");
 		addContactButtonUI= app.loadImage("../image/interactive/addContactButtonUI.png");
@@ -47,15 +49,38 @@ public class ContactRegisterView {
 		inputs = new String[5];
 		initializeTextFields();
 	}
+	
+	//add new contact Screen-------------------
+			private void initializeTextFields() {
+				inputs[0] = "Name";
+				inputs[1] = "Last Name";
+				inputs[2] = "E-mail";
+				inputs[3] = "Nationality";
+				inputs[4] = "Phone Number";
 
+				
+				cp5.addTextfield(inputs[0]).setPosition(205,252).setSize(406, 51).setAutoClear(true);
+				cp5.addTextfield(inputs[1]).setPosition(670,251).setSize(406, 51).setAutoClear(true);
+				cp5.addTextfield(inputs[2]).setPosition(205,329).setSize(406, 51).setAutoClear(true);
+				cp5.addTextfield(inputs[3]).setPosition(670,329).setSize(406, 51).setAutoClear(true);
+				cp5.addTextfield(inputs[4]).setPosition(205,405).setSize(406, 51).setAutoClear(true);
+				
+			}
+			
 	public void drawScreen() {
-		if(contactAdded==true) {
+		if(error==true ||contactAdded==true ) {
 			app.image(registerContactScreen, 0,72);
 			app.image(navBar, -4, 0);
 			app.image(addContactButton, 541, 591);
 			app.fill(0,95);
 			app.rect(0,0,1280, 720);
-			app.image(added,130, 274);
+			
+			if(contactAdded==true){
+				app.image(added,130, 274);
+			}
+			if(error==true) {
+				app.image(errorMessageC,179, 273);
+			}
 		}else {
 			app.image(registerContactScreen, 0,72);
 			app.image(navBar, -4, 0);
@@ -68,60 +93,18 @@ public class ContactRegisterView {
 
 	}
 	
-	//add new contact Screen-------------------
-		private void initializeTextFields() {
-			inputs[0] = "Name";
-			inputs[1] = "Last Name";
-			inputs[2] = "E-mail";
-			inputs[3] = "Nationality";
-			inputs[4] = "Phone Number";
-
-			
-			cp5.addTextfield(inputs[0]).setPosition(205,252).setSize(406, 51).setAutoClear(true);
-			cp5.addTextfield(inputs[1]).setPosition(670,251).setSize(406, 51).setAutoClear(true);
-			cp5.addTextfield(inputs[2]).setPosition(205,329).setSize(406, 51).setAutoClear(true);
-			cp5.addTextfield(inputs[3]).setPosition(670,329).setSize(406, 51).setAutoClear(true);
-			cp5.addTextfield(inputs[4]).setPosition(205,405).setSize(406, 51).setAutoClear(true);
-			
-		}
-		
-		private boolean addContact() {
-			boolean success=false;
-			name=cp5.get(Textfield.class, "Name").getText();
-			lastName=cp5.get(Textfield.class, "Last Name").getText();
-			email=cp5.get(Textfield.class, "E-mail").getText();
-			nationality=cp5.get(Textfield.class, "Nationality").getText();
-			phone=cp5.get(Textfield.class, "Phone Number").getText();
-			
-			
-			boolean notemptyName = !name.equals("");
-			boolean notemptyLastName = !lastName.equals("");
-			boolean notemptyEmail = !email.equals("");
-			boolean notemptyNationality = !nationality.equals("");
-			boolean notemptyPhone = !phone.equals("");
-			
-			
-			if(!notemptyName || !notemptyLastName || !notemptyEmail ||
-					!notemptyNationality||!notemptyPhone) {
-				addContactError=true;
-				System.out.println("si");
-			}
-				
-			if(success==true) {
-				cp5.get(Textfield.class, "Name").setText("");
-				cp5.get(Textfield.class, "Last Name").setText("");
-				cp5.get(Textfield.class, "E-mail").setText("");
-				cp5.get(Textfield.class, "Nationality").setText("");
-				cp5.get(Textfield.class, "Phone Number").setText("");
-				
-			}
-			return success;
-		}
-		//-----------------------
+	
 
 	public int changeScreen() {
 		int screen=9;
-		if(contactAdded==true) {
+		
+		if(error==false && addContactError==false) {
+			if(app.mouseX>541 && app.mouseX<739 &&app.mouseY>591 && app.mouseY<638) {
+				boolean success=contactRegister();
+				if(success==true) {
+					screen=9;
+				}
+				else if(contactAdded==true) {
 			if(app.mouseX>560 && app.mouseX<758 &&app.mouseY>420 && app.mouseY<467) {
 				screen=8;
 				contactAdded=false;
@@ -138,10 +121,48 @@ public class ContactRegisterView {
 				contactAdded=true;
 			}
 		}
-
-
-		return screen;
 	}
+}
+			
+		return screen;
+	
+		}
+
+	private boolean contactRegister() {
+		boolean success=false;
+		name=cp5.get(Textfield.class, "Name").getText();
+		lastName=cp5.get(Textfield.class, "Last Name").getText();
+		email=cp5.get(Textfield.class, "E-mail").getText();
+		nationality=cp5.get(Textfield.class, "Nationality").getText();
+		phone=cp5.get(Textfield.class, "Phone Number").getText();
+		
+		
+		boolean notemptyName = !name.equals("");
+		boolean notemptyLastName = !lastName.equals("");
+		boolean notemptyEmail = !email.equals("");
+		boolean notemptyNationality = !nationality.equals("");
+		boolean notemptyPhone = !phone.equals("");
+		
+		
+		if(!notemptyName || !notemptyLastName || !notemptyEmail ||
+				!notemptyNationality||!notemptyPhone) {
+			addContactError=true;
+			System.out.println("si");
+		}
+			
+		if(success==true) {
+			cp5.get(Textfield.class, "Name").setText("");
+			cp5.get(Textfield.class, "Last Name").setText("");
+			cp5.get(Textfield.class, "E-mail").setText("");
+			cp5.get(Textfield.class, "Nationality").setText("");
+			cp5.get(Textfield.class, "Phone Number").setText("");
+			
+		}
+		return success;
+	}
+	//-----------------------
+	
+	
 	
 	public ControlP5 getCp5() {
 		return cp5;
